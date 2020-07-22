42953ce043e7ddc129ab5ab01a3a4881ffbf297f
Stew <stephanbelizaire@gmail.com>
2020-07-22 00:44:52 -0400

    Flask tutorial 1-5
    In hienz sight I should have push after every tutorial, but no point crying over spilled milk.

--- app.py
+++ app.py
@@ -1,6 +1,36 @@
-def apples():
-    return print("apples")
-apples()
-def fourapples():
-    return print("4 apples")
-fourapples()
+from flask import Flask,redirect,url_for,render_template,request,session
+from flask_mysqldb import MySQL
+
+app=Flask(__name__)
+app.secret_key = "password"
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login" ,  methods=["POST", "GET"])
+def input():
+    if request.method == "POST":
+        user = request.form["nm"]
+        session["user"] = user
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            return redirect(url_for("user"))
+        return render_template("log.html")
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("input"))
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("input"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
