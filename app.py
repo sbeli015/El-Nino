d55f93a5548f403c06e87c695ad381ae93aa76d4
Stew <stephanbelizaire@gmail.com>
2020-07-22 20:55:28 -0400

    Tutorial 6
    Flash , the flash, green rash, hulk smash, skull bash, brown hash, hash brown? And now I am hungry.

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
-from flask import Flask,redirect,url_for,render_template,request,session
+from flask import Flask,redirect,url_for,render_template,request,session,flash
 from flask_mysqldb import MySQL
 
 app=Flask(__name__)
 app.secret_key = "password"
 
 @app.route("/")
+def sendhome():
+    return redirect(url_for("home"))
+
+@app.route("/home")
 def home():
     return render_template("index.html")
 
 @app.route("/login" ,  methods=["POST", "GET"])
-def input():
+def login():
     if request.method == "POST":
         user = request.form["nm"]
         session["user"] = user
+        flash("Login Successful", "info")
         return redirect(url_for("user"))
     else:
         if "user" in session:
+            flash("You are already a Member.")
             return redirect(url_for("user"))
         return render_template("log.html")
 
@@ -23,14 +29,18 @@ def input():
 def user():
     if "user" in session:
         user = session["user"]
-        return f"<h1>{user}</h1>"
+        return render_template("user.html",user=user)
     else:
-        return redirect(url_for("input"))
+        flash("You aren't a Member yet.")
+        return redirect(url_for("login"))
 
 @app.route("/logout")
 def logout():
+    if "user" in session:
+        user = session["user"]
+        flash(f"Hope you enjoyed your stay, {user}!", "info")
     session.pop("user", None)
-    return redirect(url_for("input"))
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)
